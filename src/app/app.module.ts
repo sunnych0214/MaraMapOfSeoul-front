031d857d2b80aa60dae90cf38b3539e8c82d6c9a
sunnych0214 <sunnych0214@gmail.com>
2019-09-21 01:04:02 +0900

    routing 성공 (map/place)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ViewModule } from './view/view.module';
 
+import { AppComponent } from './app.component';
 import { PlaceComponent } from './place/place.component';
 import { MapComponent } from './map/map.component';
 
@@ -15,18 +15,13 @@ import { ApiService } from './api.service';
 @NgModule({
   declarations: [
     AppComponent,
-    PlaceComponent,
-    MapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'place', component: PlaceComponent},
-      { path: 'map', component: MapComponent},
-
-    ])
+    CommonModule,
+    ViewModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 import { PlaceComponent } from '../place/place.component';
 import { MapComponent } from '../map/map.component';
+import { AppComponent } from '../app.component';
+import { ViewComponent } from './view.component';
+
+export const ROUTES:Routes = 
+[ 
+  { path: '', component: MapComponent},
+  { path: 'view', 
+    component: ViewComponent,
+    children: 
+    [ 
+      { path: 'place', component: PlaceComponent }, 
+      { path: 'map', component: MapComponent}
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
     PlaceComponent,
-    MapComponent
+    MapComponent,
+    ViewComponent
   ],
   imports: [
+    RouterModule.forRoot(ROUTES),
     CommonModule
   ]
 })
+
 export class ViewModule { }
