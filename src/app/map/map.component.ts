b0c537007b82ea9b062b4888c9cf185a14d6d9b6
jieunhong <hyy0786@naver.com>
2019-10-23 21:15:29 +0900

    2호선 css 추가 - 홍지은

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,5 +11,20 @@ export class MapComponent implements OnInit {
   constructor(private apiService : ApiService) { }
 
   ngOnInit() {
+    $(document).on('click','.subway__station', function(){
+      var station = $(this);
+      var transferId = station.attr('data-transfer-id');
+      var svg = station.closest('svg');
+      var isSelected = station.attr('class').indexOf("is-selected") > -1;
+      var elements;
+      if(transferId)
+        elements = $(this).add(svg.find('[data-transfer-id="'+transferId+'"]'));
+      else
+        elements = station;
+      if(isSelected)
+        elements.attr('class','subway__station');
+      else
+        elements.attr('class','subway__station is-selected');
+    });
   }
 }
